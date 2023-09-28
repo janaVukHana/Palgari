e6d42b0dde4ed5fc97f5a0f1a4179e2479665f82
janaVukHana <ilija009@gmail.com>
2023-09-28 23:13:05 +0200

    add animation

--- src/components/LazyDrop.tsx
+++ src/components/LazyDrop.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { useState, useEffect } from 'react'
+import { AnimationOnScroll } from 'react-animation-on-scroll'
 
 import Flex from './StyledComponents/Flex'
 import H2 from './StyledComponents/H2'
@@ -66,25 +67,35 @@ const LazyDrop = () => {
     }, []);
 
     return (
+        
         <Flex $gap $justify='start' >
             <FlexItem>
-                <H2 $color="var(--dark)" $fontSize='4rem'>Lazy Drop</H2>
+                <AnimationOnScroll animateIn="animate__fadeInLeft" duration={1.5} animateOnce={false} animatePreScroll={false}>
+                    <H2 $color="var(--dark)" $fontSize='4rem'>Lazy Drop</H2>
+                </AnimationOnScroll>
+                <AnimationOnScroll animateIn="animate__fadeInLeft" duration={2} animateOnce={false} animatePreScroll={false}>
+
                 <Paragraph $color="var(--dark-secondary)">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt adipisci assumenda sint numquam debitis 
                     odit unde placeat, incidunt odio animi sapiente asperiores alias dicta molestiae iusto eveniet 
                     consectetur exercitationem quibusdam eius minus, consequuntur qui illo rem. Delectus harum tempora 
                 </Paragraph>
+                </AnimationOnScroll>
             </FlexItem>
             <FlexItem $flex="2">
+                <AnimationOnScroll animateIn="animate__fadeInRight" duration={2} animateOnce={false} animatePreScroll={false}>
+
                 <div style={{marginBottom: '0.5rem'}}>
                     <Img src={selectedImg} alt="lazy drop" />
                 </div>
+
                 <List>
                     <ListItem $bgColor="black" onClick={() => handleClick('./images/lazyDropImg/black.png')}></ListItem>
                     <ListItem $bgColor="blue" onClick={() => handleClick('./images/lazyDropImg/blue.png')}></ListItem>
                     <ListItem $bgColor="yellow" onClick={() => handleClick('./images/lazyDropImg/yellow.png')}></ListItem>
                     <ListItem $bgColor="purple" onClick={() => handleClick('./images/lazyDropImg/purple.png')}></ListItem>
                 </List>
+                </AnimationOnScroll>
             </FlexItem>
         </Flex>
     )
